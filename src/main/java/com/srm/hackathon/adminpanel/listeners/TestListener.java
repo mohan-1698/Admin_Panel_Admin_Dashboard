a3035a9359aa964331bc3908e91465ea3c599f35
mohan-1698 <mohanvenky201@gmail.com>
2026-04-14 18:41:22 +0530

    Added extentReports

--- src/main/java/com/srm/hackathon/adminpanel/listeners/TestListener.java
+++ src/main/java/com/srm/hackathon/adminpanel/listeners/TestListener.java
@@ -3,17 +3,42 @@ package com.srm.hackathon.adminpanel.listeners;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.aventstack.extentreports.*;
+import com.srm.hackathon.adminpanel.utils.ExtentManager;
 import com.srm.hackathon.adminpanel.utils.ScreenshotUtil;
 
 public class TestListener implements ITestListener {
 
+    private static ExtentReports extent = ExtentManager.getInstance();
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest = extent.createTest(result.getName());
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().pass("Test Passed");
+    }
+
     @Override
     public void onTestFailure(ITestResult result) {
 
-        String testName = result.getName();
+        String screenshotPath = ScreenshotUtil.captureScreenshot(result.getName());
 
-        ScreenshotUtil.captureScreenshot(testName);
+        test.get().fail(result.getThrowable());
 
-        System.out.println("Screenshot captured for: " + testName);
+        try {
+            test.get().addScreenCaptureFromPath(screenshotPath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onFinish(org.testng.ITestContext context) {
+        extent.flush();
     }
 }
---
+++ src/main/java/com/srm/hackathon/adminpanel/utils/ExtentManager.java
@@ -0,0 +1,26 @@
+package com.srm.hackathon.adminpanel.utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+
+    private static ExtentReports extent;
+
+    public static ExtentReports getInstance() {
+
+        if (extent == null) {
+
+            String path = System.getProperty("user.dir") + "/reports/ExtentReport.html";
+
+            ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+            reporter.config().setReportName("Admin Panel Automation Report");
+            reporter.config().setDocumentTitle("Test Results");
+
+            extent = new ExtentReports();
+            extent.attachReporter(reporter);
+        }
+
+        return extent;
+    }
+}
